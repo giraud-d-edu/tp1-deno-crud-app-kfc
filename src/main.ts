2356cca499e56d51536ba774ea2e25f142666eb4
Amaury <amaurymechin37@gmail.com>
2025-03-03 14:45:00 +0100

    ✨ add mongoDB config

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,10 @@
 import { Application } from "https://deno.land/x/oak/mod.ts";
 import router from "./controllers/MovieController.ts";
+import { connectToDatabase } from "./config/db.ts";
 
 const app = new Application();
+
+await connectToDatabase();
 
 app.use(router.routes());
 app.use(router.allowedMethods());
