7b03dd19401ce0b1931283e007543a7f40e7070b
IceK37 <45316097+IceK37@users.noreply.github.com>
2025-03-04 11:48:29 +0100

    fixs typo

--- src/repositories/ActorRepository.ts
+++ src/repositories/ActorRepository.ts
@@ -5,8 +5,8 @@ export class ActorRepository {
     private actors: Actor[] = actors
     private nextId = actors.length + 1;
 
-    create(actor: Omit<Actor, 'id'>): Actor {
-        const newActor: Actor = {id: this.nextId++, ...actor };
+    create(actor: Omit<Actor, 'acteur_id'>): Actor {
+        const newActor: Actor = {acteur_id: this.nextId++, ...actor };
         this.actors.push(newActor);
         return newActor;
     }
@@ -16,11 +16,11 @@ export class ActorRepository {
     }
 
     getById(id: number): Actor | undefined {
-        return this.actors.find(actor => actor.id === id);
+        return this.actors.find(actor => actor.acteur_id === id);
     }
 
     deleteById(id: number): boolean {
-        const index = this.actors.findIndex(actor => actor.id === id);
+        const index = this.actors.findIndex(actor => actor.acteur_id === id);
         if (index !== -1) {
             this.actors.splice(index, 1);
             return true;
@@ -28,10 +28,10 @@ export class ActorRepository {
         return false;
     }
 
-    updateById(id: number, updatedActor: Actor): Actor | undefined {
-        const index = this.actors.findIndex(actor => actor.id === id);
+    updateById(acteur_id: number, updatedActor: Actor): Actor | undefined {
+        const index = this.actors.findIndex(actor => actor.acteur_id === acteur_id);
         if (index !== -1) {
-            this.actors[index] = { ...updatedActor, id };
+            this.actors[index] = { ...updatedActor, acteur_id };
             return this.actors[index];
         }
         return undefined;
